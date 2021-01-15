6d472b5f9b238158b8913dcd7b4087af191a990b
Layrovell <marina95prodan@gmail.com>
2021-01-15 15:35:12 +0200

    Updates

---
+++ precache-manifest.dd1020fa17360529702431a2ceaa9574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0df660e59ebee3faa939319a673568d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d90f898ad7e3bd75555",
+    "url": "./static/css/main.c8c1cf84.chunk.css"
+  },
+  {
+    "revision": "7d5666f70c00959c25fc",
+    "url": "./static/js/2.b46a62ab.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.b46a62ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d90f898ad7e3bd75555",
+    "url": "./static/js/main.b74eef38.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
